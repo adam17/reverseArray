61987cdca7503eaec614b7d533618db192a12b17
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2018-08-05 17:58:55 +0100

    First working and naive implementation plus tests
    Simply run through the array using for loop and reverse elements.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 'use strict';
 
 module.exports = {
-  reverseArray: function() {},
+  reverseArray: function(arr) {
+    if (!arr || !Array.isArray(arr))
+      return [];
 
-  reverseArrayInPlace: function() {}
+    let retArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      retArr.push(arr[i]);
+    }
+    return retArr;
+  },
+
+  reverseArrayInPlace: function(arr) {
+    if (!arr || !Array.isArray(arr))
+      return [];
+
+    let temp;
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+      temp = arr[i];
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+  }
 };
--- test.js
+++ test.js
@@ -5,23 +5,37 @@ const reverseArr = require('.');
 
 let arrA = ["A", "B", "C"];
 let arrB = [1, 2, 3, 4, 5];
+let arrC = [11, 11, 22, 44, 66, 77];
 
 describe('reverseArray takes an array as argument and produces a new array that has the same elements in the inverse order', () => {
+  it('empty response when empty parameter', () => {
+    assert.deepEqual(reverseArr.reverseArray(), []);
+    assert.deepEqual(reverseArr.reverseArray(null), []);
+    assert.deepEqual(reverseArr.reverseArray(undefined), []);
+    assert.deepEqual(reverseArr.reverseArray(''), []);
+  });
   it('reverse ["A", "B", "C"]', () => {
     assert.deepEqual(reverseArr.reverseArray(arrA), ['C', 'B', 'A']);
   });
-
   it('reverse [1, 2, 3, 4, 5]', () => {
     assert.deepEqual(reverseArr.reverseArray(arrB), [5, 4, 3, 2, 1]);
   });
 });
 
 describe('reverseArrayInPlace: it modifies the array given as argument by reversing its elements', () => {
+  it('empty response when empty parameter', () => {
+    assert.deepEqual(reverseArr.reverseArrayInPlace(), []);
+    assert.deepEqual(reverseArr.reverseArrayInPlace(null), []);
+    assert.deepEqual(reverseArr.reverseArrayInPlace(undefined), []);
+    assert.deepEqual(reverseArr.reverseArrayInPlace(''), []);
+  });
   it('reverse ["A", "B", "C"]', () => {
     assert.deepEqual(reverseArr.reverseArrayInPlace(arrA), ['C', 'B', 'A']);
   });
-
   it('reverse [1, 2, 3, 4, 5]', () => {
     assert.deepEqual(reverseArr.reverseArrayInPlace(arrB), [5, 4, 3, 2, 1]);
+  });
+  it('reverse [11, 11, 22, 44, 66, 77]', () => {
+    assert.deepEqual(reverseArr.reverseArrayInPlace(arrC), [77, 66, 44, 22, 11, 11]);
   });
 });
